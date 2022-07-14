0dc3c176c7a7825b0f37e8e60e4701755cead42b
AmrTamerfathy <you@example.com>
2022-07-14 05:35:27 +0200

    Dashboard h&m

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { MainLayoutComponent } from './components/main-layout/main-layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ProductsComponent } from './components/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    FooterComponent,
+    LoginComponent,
+    SignupComponent,
+    MainLayoutComponent,
+    NotFoundComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
